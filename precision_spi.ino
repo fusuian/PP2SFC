bb8f9869772504296f9700ba70e3953d5c24360f
ASAHI Michiharu <fusuian@gmail.com>
2017-10-08 00:01:19 +0900

    LSBFIRSTの設定でsw_data_t型で解釈成功

--- precision_spi.ino
+++ precision_spi.ino
@@ -9,7 +9,47 @@
 #define PIN_CLEAR    6
 #define PIN_MONITOR  5 
 
-volatile unsigned char buf [6];
+// data structure as sent from my sidewinder device, 48 bits total
+// this datatype is accessable via two ways: an integer-array and a struct
+// the former is used to used to manipulate the data at a bit-level
+// while the latter is used to access individual pieces of data
+typedef union
+{
+  // the struct consisting of the various data-fields sent by the sidewinder device
+  struct
+  {
+    unsigned int btn_fire:1;      // bit  1
+
+    unsigned int btn_top:1;       // bit  2
+    unsigned int btn_top_up:1;    // bit  3
+    unsigned int btn_top_down:1;  // bit  4
+
+    unsigned int btn_a:1;         // bit  5
+    unsigned int btn_b:1;         // bit  6
+    unsigned int btn_c:1;         // bit  7
+    unsigned int btn_d:1;         // bit  8
+    
+    unsigned int btn_shift:1;     // bit  9
+
+    unsigned int x:10;            // bits 10-19
+    unsigned int y:10;            // bits 20-29
+    unsigned int m:7;             // bits 30-36
+    unsigned int r:6;             // bits 37-42
+
+    unsigned int head:4;          // bits 43-46
+
+    unsigned int reserved:1;      // bit 47
+    unsigned int parity:1;        // bit 48
+
+    unsigned short blank;
+  };
+
+  // the ints are used to access the struct-data at bit-level
+  unsigned char buf[6];
+} sw_data_t;
+
+
+volatile sw_data_t sw_data;
 volatile byte pos;
 
 void setup (void)
@@ -26,6 +66,7 @@ void setup (void)
   pinMode(SCK, INPUT);
   pinMode(SS,  INPUT);
   SPCR |= _BV(SPE);
+  SPI.setBitOrder(LSBFIRST); 
   SPI.attachInterrupt();
 }
 
@@ -34,7 +75,7 @@ void setup (void)
 ISR (SPI_STC_vect)
 {
   PORTD |= _BV(PIN_MONITOR);
-  buf[pos++] = SPDR;
+  sw_data.buf[pos++] = SPDR;
   PORTD &= ~_BV(PIN_MONITOR);
 }
 
@@ -56,10 +97,20 @@ void loop (void)
   delayMicroseconds(1000); // ここで待機する間に割り込みでSPI受信
 
   for (int i=0; i < 6; i++){  
-      Serial.print(buf[i], HEX);
+      Serial.print(sw_data.buf[i], HEX);
       Serial.print(":");
   }
   Serial.println("");
+
+    Serial.print("sw_data: ");
+    Serial.print(sw_data.btn_fire);
+    Serial.print("; ");
+    Serial.print(sw_data.btn_top);
+    Serial.print("; x=");
+    Serial.print(sw_data.x - 512);
+    Serial.print("; y=");
+    Serial.print(sw_data.y - 512);
+    Serial.println();
 
   delay(50);
 }
